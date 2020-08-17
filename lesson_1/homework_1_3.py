67906989963380a3129dac8d807098bc5308bd9c
shabaiev <ilyashabaev2209@gmail.com>
2020-08-17 17:34:58 -0500

    homework_lesson1

--- lesson_1/homework_1_3.py
+++ lesson_1/homework_1_3.py
@@ -3,16 +3,18 @@ import math
 
 # Round a off to three decimal places.
 
-a = 10.04
-result_1 = None
+a = 10.04789
 
+result_1 = (round(a, 3))
+print(result_1)
 
 # Round up result_2 to greater value.
 
 result_2 = 5 / 2 * 6 + 1.25 - 4
-
+result_2 = math.ceil(result_2)
 
 # Round up result_3 to lesser value.
 
 result_3 = 8 / 3 * 5 + 4.75 - 7
+result_3 = math.floor(result_3)
 
